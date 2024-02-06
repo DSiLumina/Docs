bba8b92957f5f19c5175d15e1521858adac02489
MCKHOAHONG <77564601+MCKHOAHONG@users.noreply.github.com>
2024-02-06 15:23:25 +0000

    [Add]

---
+++ .mode.js
@@ -0,0 +1,11 @@
+const list = [
+  {
+    name: 'Highi Framework Length',
+    type: 'length',
+    file: [".*", "*.html", "*.css", "*.js", "*.json", "*.md", "*.xml"]
+  },{
+    name: 'Highi Framework Width',
+    type: 'width',
+    file: [".*", "*.html", "*.css", "*.js", "*.json", "*.md", "*.xml"]
+  }
+]
